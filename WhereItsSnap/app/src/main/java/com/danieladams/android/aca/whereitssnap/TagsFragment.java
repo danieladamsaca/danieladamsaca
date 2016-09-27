b86b099daded0fe54f0b59f28a5364ff4a4e00c1
DanielA <obsidian151.da@gmail.com>
2016-09-27 09:14:47 -0500

    sep,27/16

---
+++ WhereItsSnap/app/src/main/java/com/danieladams/android/aca/whereitssnap/TagsFragment.java
@@ -0,0 +1,19 @@
+package com.danieladams.android.aca.whereitssnap;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+
+/**
+ * Created by danieladams on 9/26/16.
+ */
+
+public class TagsFragment extends ListFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
+
